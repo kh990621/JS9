bfcfd783533d3ae0e0196db67319a9f86f2710bd
kh990621 <hitoshi990621@icloud.com>
2024-11-09 20:15:11 +0900

    js11

--- js9.js
+++ js9.js
@@ -2,14 +2,28 @@ document.addEventListener('DOMContentLoaded', function() {
     const pullDownButton = document.getElementById("lists");
 
     pullDownButton.addEventListener('mouseover', function() {
-        pullDownButton.setAttribute("style", "background-color:blue")
+        this.setAttribute("style", "background-color:blue")
     });
 
     pullDownButton.addEventListener('click', function() {
-        pullDownButton.setAttribute("style", "background-color:green")
+        this.setAttribute("style", "background-color:green")
     });
 
     pullDownButton.addEventListener('mouseleave', function() {
-        pullDownButton.removeAttribute("style", "background-color:red")
+        this.removeAttribute("style", "background-color:red")
     });
 });
+
+
+const pullDownButton = document.getElementById("lists")
+const pullDownParents = document.getElementById("pull-down")
+
+pullDownButton.addEventListener("click", function() {
+    if (pullDownParents.getAttribute("style") == "display:block;"){
+        pullDownParents.removeAttribute("style", "display:block;")
+        console.log("非表示")
+    } else {
+    pullDownParents.setAttribute("style", "display:block;")
+    console.log("表示）")
+    }
+})
